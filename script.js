3aa856663f59b46f20c47c45a4498730e9c0c5c5
mihailomitrovic <75587799+mihailomitrovic@users.noreply.github.com>
2020-12-08 23:01:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var r1v = document.getElementById("r1").value;
+var r2v = document.getElementById("r2").value;
+var r3v = document.getElementById("r3").value;
+
+function prozor() {
+    if (r1v.value == "") {
+        alert("Fali vam ime.");
+        return false;
+    }
+    if (r2v.value == "") {
+        alert("Fali vam email.");
+        return false;
+    }
+    else if (r3v.value == "") {
+        alert("Fali vam komentar.");
+        return false;
+    }
+    else if (r1v.value == "") {
+        alert("Fale vam ime i email.");
+        return false;
+    }
+    else if (r1v.value == null && r3v.value == null) {
+        alert("Fale vam ime i komentar.");
+        return false;
+    }
+    else if (r2v.value == null && r3v.value == null) {
+        alert("Fale vam email i komentar.");
+        return false;
+    }
+    else if (r1v.value == null && r2v.value == null && r3v.value == null) {
+        alert("Fale vam sva tri elementa.");
+        return false;
+    }
+}
